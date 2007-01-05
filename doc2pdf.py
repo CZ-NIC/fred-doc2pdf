3bb5b46b36175b09f455e94c29170262c40d39fc
Zdeněk Böhm <zdenek.bohm@nic.cz>
2007-01-05 12:12:15 +0000

    pridana funkce vkladani hodnoty celkoveho poctu stran
    git-svn-id: file:///data/svn/enum/fred2pdf/trunk@2288 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

--- doc2pdf.py
+++ doc2pdf.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 """
-This module is frontend of the Tiny RML2PDF system (http://openreport.org/) 
+This module is running on the Tiny RML2PDF system (http://openreport.org/) 
 and  The ReportLab Open Source PDF library (the ReportLab Toolkit) 
 http://www.reportlab.org/.
 
@@ -35,51 +35,64 @@ import sys
 import StringIO
 
 import reportlab
-import trml2pdf
+from trml2pdf import trml2pdf, utils
 
 # Need for register TrueType fonts
 from reportlab.pdfbase.ttfonts import TTFont
+from reportlab.pdfgen import canvas
 
 import configuration
 
 
-# Init paths
+# Init paths of the TrueType fonts
 reportlab.rl_config.TTFSearchPath = configuration.TrueTypePath
 
 # Default encoding (used for TrueType). Don't touch.
-trml2pdf.trml2pdf.encoding = 'UTF-8'
-
+trml2pdf.encoding = 'UTF-8'
 
 def attr_get(node, attrs, dict={}):
-  """Overwrite function attr_get() in module utils. We need encode unicodes.
-  """
-  res = {}
-  for name in attrs:
-    if node.hasAttribute(name):
-      res[name] = trml2pdf.utils.unit_get(node.getAttribute(name))
-  for key in dict:
-    if node.hasAttribute(key):
-      if dict[key]=='str':
-        value = node.getAttribute(key)
-        if type(value) is unicode: value = value.encode(trml2pdf.trml2pdf.encoding)
-        res[key] = str(value)
-      elif dict[key]=='bool':
-        res[key] = trml2pdf.utils.bool_get(node.getAttribute(key))
-      elif dict[key]=='int':
-        res[key] = int(node.getAttribute(key))
-  return res
+    """Overwrite function attr_get() in module utils. We need encode unicodes.
+    This HOOK fix problem with unicode.
+    """
+    res = {}
+    for name in attrs:
+        if node.hasAttribute(name):
+            res[name] = utils.unit_get(node.getAttribute(name))
+    for key in dict:
+        if node.hasAttribute(key):
+            if dict[key]=='str':
+                    # res[key] = str(node.getAttribute(key))
+                    # we need encode unicode to local encoding:
+                    value = node.getAttribute(key)
+                    if type(value) is unicode: value = value.encode(trml2pdf.encoding)
+                    res[key] = str(value)
+            elif dict[key]=='bool':
+                res[key] = utils.bool_get(node.getAttribute(key))
+            elif dict[key]=='int':
+                res[key] = int(node.getAttribute(key))
+    return res
+utils.attr_get = attr_get
 
-# Overwritting default function. We need encode unicode texts.
-trml2pdf.utils.__dict__['attr_get'] = attr_get
+original_paragraph_setup = reportlab.platypus.paragraph.Paragraph._setup
+def fred_paragraph_setup(self, text, style, bulletText, frags, cleaner):
+    """Hook original function for resolve problem with occurence character & 
+    during parsing XML code.
+    """
+    original_paragraph_setup(self, text.replace('&','&amp;'), style, bulletText, frags, cleaner)
+reportlab.platypus.paragraph.Paragraph._setup = fred_paragraph_setup
 
-def registerTTFFontFamilly(names):
+"""
+Set of functions for register TrueType fonts.
+"""
+
+def register_TTF_font_familly(names):
     """Register TrueType font. Names is list of tuples (PostSriptName, FileName.ttf)
     The list must be valid - normal (first) item must be set. Other types
     are obligatory.
     names = ((name, file), ...) four items in order: '','Bold','Italic','BoldItalic'
     """
     if len(names) != 4:
-        raise 'Internal Error in registerTTFFontFamilly: The list MUST have 4 items: %s'%str(names)
+        raise 'Internal Error in register_TTF_font_familly(): The list MUST have 4 items: %s'%str(names)
     for n in range(4):
         name, file = names[n]
         if name: reportlab.pdfbase.pdfmetrics.registerFont(TTFont(name, file))
@@ -108,67 +121,141 @@ def docinit_TTF_elements(element_name, elements):
                     if file == '': raise 'Element docinit.%s attribute main fontFile missing.'%element_name
                 if (name and not file) or (file and not name):
                     raise "Element docinit.registerFont missing one of attributes: fontName%s='%s' fontFile%s='%s'"%(suffix[n], name, suffix[n], file)
-            registerTTFFontFamilly(names)
+            register_TTF_font_familly(names)
     return is_element_found
 
+def docinit_TTF(els):
+    'Register TTF font family.'
+    if not docinit_TTF_elements('registerDefaultFont', els):
+      # if template has not defined default font name
+      # we use default names from this module
+      register_TTF_font_familly(configuration.DEFAULT_STYLE_FONT)
+    docinit_TTF_elements('registerFont', els)
 
-class ttf_rml_doc(trml2pdf.trml2pdf._rml_doc):
-    'Modify clas for using TrueType'
-    def __init__(self, data):
-        trml2pdf.trml2pdf._rml_doc.__init__(self, data)
+"""
+*** HOOK OF THE REGISTER FONTS ***
+If your documents works with unicode, you need use TrueType fonts.
+Here is hook for register TTF indo document.
+"""
 
-    def docinit_TTF(self, els):
-        'Register TTF font family.'
-        if not docinit_TTF_elements('registerDefaultFont', els):
-          # if template has not defined default font name
-          # we use default names from this module
-          registerTTFFontFamilly(configuration.DEFAULT_STYLE_FONT)
-        docinit_TTF_elements('registerFont', els)
+original_rml_doc_render = trml2pdf._rml_doc.render
+def fred_render(self, out):
+    el = self.dom.documentElement.getElementsByTagName('docinit')
+    if el:
+        docinit_TTF(el)
+    else:
+        register_TTF_font_familly(configuration.DEFAULT_STYLE_FONT)
+    original_rml_doc_render(self, out)
+trml2pdf._rml_doc.render = fred_render
 
-    def docinit(self, el):
-      'Disable parent docinit()'
+def fred_docinit(self, els):
+    'Disable original function for register fonts.'
+trml2pdf._rml_doc.docinit = fred_docinit
 
-    def render(self, out):
-      'Overwrite parent render()'
-      # register TTF fonts...
-      el = self.dom.documentElement.getElementsByTagName('docinit')
-      if el:
-        self.docinit_TTF(el)
-      else:
-        registerTTFFontFamilly(configuration.DEFAULT_STYLE_FONT)
-      # ...and continue in previous way
-      trml2pdf.trml2pdf._rml_doc.render(self, out)
+"""
+*** HOOK OF THE TOTAL PAGE NUMBER ***
+
+This code HOOKS modules reportlab and trml2pdf.
+These modules in actual versions have not implemented count value of the total page number.
+So we need make some hooks until original modules will not updated.
+
+reportlab$ svn info
+Path: .
+URL: http://www.reportlab.co.uk/svn/public/reportlab/trunk/reportlab
+Repository Root: http://www.reportlab.co.uk/svn/public
+Repository UUID: 636e621b-80dc-0310-9e7d-ef5f34d1b73f
+Revision: 3004
+Node Kind: directory
+Schedule: normal
+Last Changed Author: rgbecker
+Last Changed Rev: 3004
+Last Changed Date: 2006-12-19 15:50:51 +0100 (\u00dat, 19 pro 2006)
+
+"""
+
+# Global variable keep number of the total page:
+num_total_page = 0
+# Definition of the MARK what represent place where will be put total page number.
+# Decimal   Octal   Hex    Value
+# 028          034      01C    FS    (File Separator)
+FS = 28
+mark_count_page_number = chr(FS)
+mark_count_page_number_octal = r'\0%o'%FS
+
+def fred_textual(self, node):
+    """Add tag of the Total page number.
+    This hook implements new tag pageNumberTotal.
+    """
+    rc = ''
+    for n in node.childNodes:
+        if n.nodeType == n.ELEMENT_NODE:
+            if n.localName=='pageNumber':
+                rc += str(self.canvas.getPageNumber())
+            # HOOK: this two lines add new tag for input total page number
+            # Warning: If the text in printed as center ro right alignment, it has no effect
+            # and number will be align on the same position as an internal mark.
+            # TODO: Find better way of the implementation (or wait for next release?)
+            elif n.localName=='pageNumberTotal':
+                rc += mark_count_page_number
+        elif (n.nodeType == node.CDATA_SECTION_NODE):
+            rc += n.data
+        elif (n.nodeType == node.TEXT_NODE):
+            rc += n.data
+    return rc.encode(trml2pdf.encoding)
+trml2pdf._rml_canvas._textual = fred_textual
 
 
-def parseString(data, fout=None):
-  'Create class ttf_rml_doc() and render data.'
-  r = ttf_rml_doc(data) # here is reason why we need this function.
-  if fout:
-    fp = file(fout,'wb')
-    r.render(fp)
-    fp.close()
-    return fout
-  else:
-    fp = StringIO.StringIO()
-    r.render(fp)
-    return fp.getvalue()
+original_BaseDocTemplate_endBuild = reportlab.platypus.BaseDocTemplate._endBuild
+def save_num_total_pages(self):
+    """Save number of the total pages.
+    This hook saves the number of pages for use later in the creating document process.
+    """
+    global num_total_page
+    num_total_page = self.page
+    original_BaseDocTemplate_endBuild(self)
+reportlab.platypus.BaseDocTemplate._endBuild = save_num_total_pages
+
+original_PDFPage_check_format = reportlab.pdfbase.pdfdoc.PDFPage.check_format
+def load_num_total_pages(self, document):
+    """Load number of the total pages.
+    Befor output data repalce all occurences of the TotalPage mark by real number.
+    """
+    self.stream = self.stream.replace(mark_count_page_number_octal, str(num_total_page))
+    original_PDFPage_check_format(self, document)
+reportlab.pdfbase.pdfdoc.PDFPage.check_format = load_num_total_pages
 
 
 def genpdf_help():
-	print 'Usage: doc2pdf input.rml >output.pdf'
-	print 'Render the standard input (RML) and output a PDF file'
-	sys.exit(0)
+    print """Usage: doc2pdf input.rml >output.pdf
+Render the standard input (RML) and output a PDF file.
+For test use:
+$ doc2pdf --test input.rml
+"""
+    sys.exit(0)
+
+def test(data):
+    'For test only. This is good for hide PDF binary source.'
+    r = trml2pdf._rml_doc(data)
+    fp = StringIO.StringIO()
+    r.render(fp)
+    return '*** END ***' # fp.getvalue()
 
 if __name__=="__main__":
   if sys.stdin.isatty():
     if len(sys.argv)>1:
         if sys.argv[1]=='--help':
             genpdf_help()
-        print parseString(file(sys.argv[1], 'r').read())
+        elif sys.argv[1]=='--test':
+            if len(sys.argv)>2:
+                print test(file(sys.argv[2], 'r').read())
+            else:
+                print 'Missing test file. Usage: doc2pdf --test input.rml' 
+        else:
+            print trml2pdf.parseString(file(sys.argv[1], 'r').read())
     else:
         print 'Usage: doc2pdf input.rml > output.pdf'
         print 'Try \'doc2pdf --help\' for more information.'
   else:
     # module in pipe:
-    print parseString(sys.stdin.read())
+    print trml2pdf.parseString(sys.stdin.read())
 
