b2afb3c391f4e2e1aab91a889d9356037ac61528
jsadek <jsadek@b60c293d-5e0d-0410-87b2-fb9704ccd4b5>
2012-03-26 14:10:32 +0000

    ChangeLog update and version bump
    git-svn-id: file:///data/svn/enum/doc2pdf/trunk@20191 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

--- setup.py
+++ setup.py
@@ -296,7 +296,7 @@ def main(directory):
                 author='Zdenek Bohm, CZ.NIC',
                 author_email='zdenek.bohm@nic.cz',
                 url='http://fred.nic.cz',
-                version='2.3.3',
+                version='2.3.4',
                 license='GNU GPL',
                 long_description='The module of the FRED system',
                 cmdclass={
