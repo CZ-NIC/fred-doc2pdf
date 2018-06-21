141e85b7b227586e4c430b60da571f8e9fcc3f24
Vlastimil Zíma <vlastimil.zima@nic.cz>
2018-06-21 09:16:17 +0200

    Ticket #22094 - Use font headers instead of config

--- setup.py
+++ setup.py
@@ -5,8 +5,6 @@ Usage:
 """
 import os
 import re
-import sys
-from subprocess import Popen, PIPE
 
 from freddist.command.install import install
 from freddist.core import setup
@@ -18,163 +16,9 @@ PACKAGE_NAME = 'fred-doc2pdf'
 
 CONFIG_FILENAME = 'fred-doc2pdf.conf'
 
-# ----------------------------------------
-# Here you can configure config variables:
-# ----------------------------------------
-
-# Folder where setup looks for font
-FONT_ROOT = '/usr/share/fonts'
-
-# Folder names where process looking for fonts
-PREFERRED_FONT_FOLDERS = ('freefont', 'truetype', 'freefont-ttf', "dejavu")
-
-# Font names which process looking for
-PREFERRED_FONT_NAMES = ('FreeSans', 'FreeSerif', 'FreeMono', 
-        'DejaVuSans', 'DejaVuSerif',
-        'Helvetica', 'Arial', 'Times', 'Times-Roman', 'Times-New-Roman')
-
-# ----------------------------------------
-def get_popen_result(command):
-    """
-    Returns result from shell command
-    """
-    popen = Popen(command, stdout=PIPE, stderr=PIPE)
-    output, error = popen.communicate()
-    if popen.returncode:
-        sys.stderr.write("Command '%s' returned exit code '%d'\n" % command, popen.returncode)
-        exit(1)
-    if error:
-        sys.stderr.write("Command '%s' returned error '%s'\n" % command, error)
-        exit(1)
-    return output
-
-
-def find_font_folders():
-    """Returns list of folders"""
-    sys.stdout.write("Looking for folders '%s'...\n" % "', '".join(PREFERRED_FONT_FOLDERS))
-    cmd = ['find', FONT_ROOT, '-type', 'd', '-iname', PREFERRED_FONT_FOLDERS[0]]
-    for name in PREFERRED_FONT_FOLDERS[1:]:
-        cmd.extend(['-or', '-iname', name])
-
-    # sys.stdout.write('$ %s\n'%cmd) info about shell command
-    data = get_popen_result(cmd)
-    data = data.strip()
-
-    # list of path with font folders:
-    font_folders = re.split('\s+', data)
-    print 'Found %d folder(s).' % len(font_folders)
-    return font_folders
-
-
-def join_font_types(plain, fontnames):
-    """
-    Chceck if name has all types: plain, obligue, bold, bold-obligue.
-    If is it true, returns string with this names.
-    """
-    font_family = ''
-    font_name, dummy_ext = plain.split('.')
-
-    obligue, bold, bold_obligue = ['']*3
-
-    for name in fontnames:
-        if re.match('%s-?(Oblique|Italics?).ttf'%font_name, name, re.I):
-            obligue = name
-        if re.match('%s-?Bold.ttf'%font_name, name,  re.I):
-            bold = name
-        if re.match('%s-?Bold-?(Oblique|Italics?).ttf'%font_name, name, re.I):
-            bold_obligue = name
-
-    if obligue and bold and bold_obligue:
-        font_family = ' '.join((plain,  obligue,  bold,  bold_obligue))
-        sys.stdout.write("Found font family '%s'\n"%plain)
-
-    return font_family
-
-
-def find_font_family(font_folders):
-    """
-    Returns path to the font and font family names (4 names)
-    """
-    font_folder_name, font_family = None, None
-    font_filenames = ['%s.ttf' % name for name in PREFERRED_FONT_NAMES]
-
-    for folder_name in font_folders:
-        data = get_popen_result(['find', folder_name, '-name', '*.ttf'])
-
-        # keep paths and names of fonts separately:
-        font_paths = []
-        font_names = []
-        for path in re.split('\s+', data.strip()):
-            font_paths.append(os.path.dirname(path))
-            font_names.append(os.path.basename(path))
-
-        print "Found %d fonts in folder '%s'." % (len(font_names), folder_name)
-        # find font family
-        # looking for fonty types (4 types = one family)
-        for font_type in font_filenames:
-            if font_type in font_names:
-                font_family = join_font_types(font_type, font_names)
-                if font_family:
-                    font_folder_name = font_paths[font_names.index(font_type)]
-                    break
-    return font_folder_name, font_family
-
-
-def find_font_path_and_family():
-    """Find truetype fonts"""
-    font_folders = find_font_folders()
-    return find_font_family(font_folders)
-
 
 class Install(install):
     description = "Install fred-doc2pdf"
-
-    user_options = install.user_options
-    user_options.append(('font-names=', None,
-        'default names for true-type fonts'))
-    user_options.append(('font-path=', None,
-        'path to true-type fonts'))
-
-    def initialize_options(self):
-        install.initialize_options(self)
-        self.font_names = None
-        self.font_path  = None
-
-    def finalize_options(self):
-        install.finalize_options(self)
-
-        # Set up fonts
-        if not self.font_path and not self.font_names:
-            self.font_path, self.font_names = find_font_path_and_family()
-        else:
-            font_names = None
-            if not self.font_path:
-                self.font_path, font_names = find_font_path_and_family()
-
-            if not self.font_names:
-                if font_names:
-                    self.font_names = font_names
-                else:
-                    dummy, self.font_names = find_font_path_and_family()
-
-            if self.no_check_deps is None:
-                # Check font path
-                if os.path.isdir(self.font_path):
-                    sys.stderr.write("Directory '%s' not found.\n" % self.font_path)
-                    exit(1)
-                # check explicit font names
-                for font_file in re.split("\s+", self.font_names):
-                    path = os.path.join(self.font_path, font_file)
-                    if not os.path.isfile(path):
-                        sys.stderr.write("File '%s' not found.\n" % path)
-                        exit(1)
-
-    def update_config(self, filename):
-        content = open(filename).read()
-        content = content.replace('TRUE_TYPE_PATH', self.font_path)
-        content = content.replace('DEFAULT_FONT_TTF', self.font_names)
-        open(filename, 'w').write(content)
-        self.announce("File '%s' was updated" % filename)
 
     def update_script(self, filename):
         content = open(filename).read()
@@ -203,8 +47,7 @@ def main():
           scripts=[PACKAGE_NAME],
           data_files=data_files,
           cmdclass={'install':Install},
-          modify_files={'$scripts/%s' % PACKAGE_NAME: 'update_script',
-                        '$sysconf/fred/%s' % CONFIG_FILENAME: 'update_config'})
+          modify_files={'$scripts/%s' % PACKAGE_NAME: 'update_script'})
 
 
 if __name__ == '__main__':
